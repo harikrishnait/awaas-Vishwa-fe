da4eb8107418f41dbf2ca0bed7417a352460822f
harikrishnait <caharikrishnag@gmail.com>
2023-08-03 07:41:46 +0530

    refactor: Header & Footer

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <div>
+      <div className="footer-container"></div>
+      <p className="footer-text">Made with Heart by <a href="https://github.com/harikrishnait"><u><i>Hari Krishna</i></u></a> in India </p>
+      </div>
+    </footer>
+      
   )
 }
 
--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Logo from '../../assets/logo-named.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="header-container">
+        <div className="header-left">
+          <Link to="/" className="header-left-brand">
+            <img src={Logo} alt="" />
+            </Link>
+            <h2>Awaas Vishwa</h2>
+          
+        </div>
+        <div className="header-right">
+          <div className="header-right-login">
+            <Link to="/login">Login</Link>
+            <Link to="/register">register</Link>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
