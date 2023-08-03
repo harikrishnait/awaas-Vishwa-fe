1a788977aed5ff0c5adf9357464c99779f1c71f3
harikrishnait <caharikrishnag@gmail.com>
2023-08-03 08:42:45 +0530

    feat: register form ui

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,24 @@
+import { Margin } from '@mui/icons-material'
+import { FormControl, TextField,Button } from '@mui/material'
 import React from 'react'
 
 const RegisterPage = () => {
   return (
     <div>
-      register
+      <div className="register-page"></div>
+      <div className="register-container">
+        <div className="register-form">
+          <h1>Sign Up</h1>
+        <FormControl fullWidth >
+        <TextField  id='filled-basic' label="Full Name" variant='filled'/>
+        <TextField  id='filled-basic' label="Phone" type="number" variant='filled'/>
+        <TextField  id='filled-basic' label="email" variant='filled'/>
+        <TextField  id='filled-basic' label="Username" variant='filled'/>
+        <TextField  id='filled-basic' label="password" type="password" variant='filled'/>
+        <Button variant="contained" sx={{marginTop:"20px"}} >Sign Up</Button>
+        </FormControl>
+        </div>
+      </div>
     </div>
   )
 }
